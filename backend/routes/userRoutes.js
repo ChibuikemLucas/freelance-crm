55f3a6ee11f77dab4abac812c046c8805b37346a
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-09-05 13:19:55 +0100

    feat: Edit user route.
    Signed-off-by: ChibuikemLucas <chibuikemlucas@gmail.com>

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const authMiddleware = require("../middleware/authMiddleware");
-const User = require("../models/User");
+const authMiddleware = require("../backend/middleware/authMiddleware");
+const User = require("../backend/models/User");
 
 const router = express.Router();
 
